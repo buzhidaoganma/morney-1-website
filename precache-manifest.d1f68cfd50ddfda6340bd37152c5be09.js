6cf2cfed7f36db34c3316b42a78223b932f1d22d
郑李畴 <2678646597@qq.com>
2022-07-10 17:08:32 +0800

    deploy

---
+++ precache-manifest.d1f68cfd50ddfda6340bd37152c5be09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e7c56e00169938b342",
+    "url": "/morney-1-website/css/app.adb88145.css"
+  },
+  {
+    "revision": "d2eabb8d7e5d633dcee0f77a94132deb",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "d4e7c56e00169938b342",
+    "url": "/morney-1-website/js/app.87f0b354.js"
+  },
+  {
+    "revision": "f9708dc78fcc9ad79e24",
+    "url": "/morney-1-website/js/chunk-vendors.9d60090e.js"
+  },
+  {
+    "revision": "38c86ee580c0f0227a68bad3d0663039",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
